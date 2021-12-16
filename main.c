0c6435533849dc1890189afebce2ee3ca4071b77
K1A2 <jckim0307@gmail.com>
2021-12-16 15:09:15 +0900

    all

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(void) {
         ExpressHeadTail *exp = read_and_anlyze(strs[i]); // 파일에서 식 읽어온 후 구조체로 변환 후 반환
         if (exp != NULL) { // *exp가 null이면 식에 오류가 있음을 뜻함
             print_all(&exp); // 변환 된 식을 출력
+            putchar('\n');
             exp = infix_to_postfix(&exp); // 중위식 후위식으로 변환
             print_all(&exp); // 후위식으로 변환된 식 출력
             Number *number = calculation(&exp); // 후위식 계산
