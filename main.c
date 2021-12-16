9d575767829e2c7f6172b37d33e870a8806b5a99
K1A2 <jckim0307@gmail.com>
2021-12-17 01:38:59 +0900

    update

--- main.c
+++ main.c
@@ -12,14 +12,16 @@ int main(void) {
     for (int i = 0;i < 1;i++) {
         ExpressHeadTail *exp = read_and_anlyze(strs[i]); // 파일에서 식 읽어온 후 구조체로 변환 후 반환
         if (exp != NULL) { // *exp가 null이면 식에 오류가 있음을 뜻함
+            printf("\n입력된 식: ");
             print_all(&exp); // 변환 된 식을 출력
             putchar('\n');
             exp = infix_to_postfix(&exp); // 중위식 후위식으로 변환
+            printf("\n중위식으로 변환된 식: ");
             print_all(&exp); // 후위식으로 변환된 식 출력
             Number *number = calculation(&exp); // 후위식 계산
            if (number != NULL) {
                // 계산에 오류가 없다면 계산 결과 출력
-               printf("\nAnswer: ");
+               printf("\n답: ");
                print_numbers(number);
                putchar('\n');
                release_numbers(&number);
